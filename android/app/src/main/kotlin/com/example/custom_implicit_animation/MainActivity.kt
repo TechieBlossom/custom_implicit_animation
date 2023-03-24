aad18dfe984909eb821b0af3a76752560e6b9e80
Prateek Sharma <programmersine@gmail.com>
2023-03-24 16:11:23 +0530

    Add explicit animation for square to rectangle

---
+++ android/app/src/main/kotlin/com/example/custom_implicit_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.custom_implicit_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:custom_implicit_animation/explicit_animation/explicit_animation_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      home: const ExplicitAnimationScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
